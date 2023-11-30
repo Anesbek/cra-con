8ed539eaf67620d20e925b5a1730dcb9b9a68b9d
Anesbek <145551912+Anesbek@users.noreply.github.com>
2023-11-30 17:55:47 +0500

    comit-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import { StyleBtn } from './components/Button.styled';
+import { Menu } from './components/Menu';
+import { MYtheme } from './components/styles/TheneStyled';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aut cumque deleniti excepturi ratione sit tempore temporibus tenetur. Assumenda aut doloremque harum iusto modi natus nemo, provident rerum soluta voluptatibus.</p>
+      
+
+    
+    
+      <BOX>
+       <StyleBtn color ={MYtheme.colors.primary} primary>Hello</StyleBtn>
+       <StyleBtn color ={MYtheme.colors.secondary} outlined>Hello</StyleBtn>
+      </BOX>
+     
     </div>
   );
 }
 
 export default App;
+
+const styleh1 = styled.h1`
+color: red;
+`
+
+
+
+const BOX = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+
+button {
+  cursor: pointer;
+}
+
+
+@media ${MYtheme.media.tablet} {
+  flex-direction: column;
+}
+
+`
